35d05cb70dd52723cb01a81bec6efec42d7ddad7
Miguel <47696065+MegRov@users.noreply.github.com>
2020-04-19 14:26:34 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 
-  define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '' );             // Set database password
-  define( 'DB_NAME', 'fruver_inv' );        // Set database name
+  define( 'DB_HOST', 'buxdwg4tgjsejrdrzoll-mysql.services.clever-cloud.com' );          // Set database host
+  define( 'DB_USER', 'uhqgfcf7nzk2pkcp' );             // Set database user
+  define( 'DB_PASS', 'iuQJsm3abn1V6DURgtx7' );             // Set database password
+  define( 'DB_NAME', 'buxdwg4tgjsejrdrzoll' );        // Set database name
 
 ?>
