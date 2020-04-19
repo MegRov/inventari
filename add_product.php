b6f82bb3109429f40a9455fc25e992d349d70f76
Miguel Guevara <lxmxg.96@outlook.com>
2020-04-19 14:20:48 -0500

    commit

---
+++ add_product.php
@@ -0,0 +1,234 @@
+<?php
+  $page_title = 'Agregar producto';
+  require_once('includes/load.php');
+  // Checkin What level user has permission to view this page
+  page_require_level(2);
+  $all_categories = find_all('categories');
+  $all_photo = find_all('media');
+?>
+<?php
+//update user image
+  if(isset($_POST['submit'])) {
+  $photo = new Media();
+  $user_id = (int)$_POST['user_id'];
+  $photo->upload($_FILES['file_upload']);
+  if($photo->process_user($user_id)){
+    $session->msg('s','La foto fue subida al servidor.');
+    redirect('edit_account.php');
+    } else{
+      $session->msg('d',join($photo->errors));
+      redirect('edit_account.php');
+    }
+  }
+?>
+<?php
+ if(isset($_POST['add_product'])){
+   $req_fields = array('product-title','product-categorie','product-quantity','buying-price', 'saleing-price' );
+   validate_fields($req_fields);
+   if(empty($errors)){
+     $p_name  = remove_junk($db->escape($_POST['product-title']));
+     $p_cat   = remove_junk($db->escape($_POST['product-categorie']));
+     $p_qty   = remove_junk($db->escape($_POST['product-quantity']));
+     $p_buy   = remove_junk($db->escape($_POST['buying-price']));
+     $p_sale  = remove_junk($db->escape($_POST['saleing-price']));
+     if (is_null($_POST['product-photo']) || $_POST['product-photo'] === "") {
+       $media_id = '0';
+     } else {
+       $media_id = remove_junk($db->escape($_POST['product-photo']));
+     }
+     $date    = make_date();
+     $query  = "INSERT INTO products (";
+     $query .=" name,quantity,buy_price,sale_price,categorie_id,media_id,date";
+     $query .=") VALUES (";
+     $query .=" '{$p_name}', '{$p_qty}', '{$p_buy}', '{$p_sale}', '{$p_cat}', '{$media_id}', '{$date}'";
+     $query .=")";
+     $query .=" ON DUPLICATE KEY UPDATE name='{$p_name}'";
+     if($db->query($query)){
+       $session->msg('s',"Producto agregado exitosamente. ");
+       redirect('add_product.php', false);
+     } else {
+       $session->msg('d',' Lo siento, registro falló.');
+       redirect('product.php', false);
+     }
+
+   } else{
+     $session->msg("d", $errors);
+     redirect('add_product.php',false);
+   }
+
+ }
+
+?>
+<?php include_once('layouts/header.php'); ?>
+<div class="row">
+  <div class="col-md-12">
+    <?php echo display_msg($msg); ?>
+  </div>
+</div>
+  <div class="row">
+  <div class="col-md-9">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <strong>
+            <span class="glyphicon glyphicon-th"></span>
+            <span>Agregar producto</span>
+         </strong>
+        </div>
+        <div class="panel-body">
+         <div class="col-md-12">
+          <form method="post" action="add_product.php" class="clearfix">
+              <div class="form-group">
+                <div class="input-group">
+                  <span class="input-group-addon">
+                   <i class="glyphicon glyphicon-th-large"></i>
+                  </span>
+                  <input type="text" class="form-control" name="product-title" placeholder="Descripción">
+               </div>
+              </div>
+              <div class="form-group">
+                <div class="row">
+                  <div class="col-md-6">
+                    <select class="form-control" name="product-categorie">
+                      <option value="">Selecciona una categoría</option>
+                    <?php  foreach ($all_categories as $cat): ?>
+                      <option value="<?php echo (int)$cat['id'] ?>">
+                        <?php echo $cat['name'] ?></option>
+                    <?php endforeach; ?>
+                    </select>
+                  </div>
+                  <div class="col-md-6">
+                    <select class="form-control" name="product-photo">
+                      <option value="">Selecciona una imagen</option>
+                    <?php  foreach ($all_photo as $photo): ?>
+                      <option value="<?php echo (int)$photo['id'] ?>">
+                        <?php echo $photo['file_name'] ?></option>
+                    <?php endforeach; ?>
+                    </select>
+                  </div>
+                </div>
+              </div>
+
+              <div class="form-group">
+               <div class="row">
+                 <div class="col-md-4">
+                   <div class="input-group">
+                     <span class="input-group-addon">
+                      <i class="glyphicon glyphicon-shopping-cart"></i>
+                     </span>
+                     <input type="number" class="form-control" name="product-quantity" placeholder="Cantidad">
+                  </div>
+                 </div>
+                 <div class="col-md-4">
+                   <div class="input-group">
+                     <span class="input-group-addon">
+                       <i class="glyphicon glyphicon-usd"></i>
+                     </span>
+                     <input type="number" class="form-control" name="buying-price" placeholder="Precio de compra">
+                     <span class="input-group-addon">.00</span>
+                  </div>
+                 </div>
+                  <div class="col-md-4">
+                    <div class="input-group">
+                      <span class="input-group-addon">
+                        <i class="glyphicon glyphicon-usd"></i>
+                      </span>
+                      <input type="number" class="form-control" name="saleing-price" placeholder="Precio de venta">
+                      <span class="input-group-addon">.00</span>
+                   </div>
+                  </div>
+               </div>
+              </div>
+              <button type="submit" name="add_product" class="btn btn-danger">Agregar producto</button>
+          </form>
+         </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php include_once('layouts/footer.php'); ?>
+
+<?php
+  $page_title = 'Lista de imagenes';
+  require_once('includes/load.php');
+  // Checkin What level user has permission to view this page
+  page_require_level(2);
+?>
+<?php $media_files = find_all('media');?>
+<?php
+  if(isset($_POST['submit'])) {
+  $photo = new Media();
+  $photo->upload($_FILES['file_upload']);
+    if($photo->process_media()){
+        $session->msg('s','Imagen subida al servidor.');
+        redirect('media.php');
+    } else{
+      $session->msg('d',join($photo->errors));
+      redirect('media.php');
+    }
+
+  }
+
+?>
+<?php include_once('layouts/header.php'); ?>
+     <div class="row">
+        <div class="col-md-6">
+          <?php echo display_msg($msg); ?>
+        </div>
+
+      <div class="col-md-12">
+        <div class="panel panel-default">
+          <div class="panel-heading clearfix">
+            <span class="glyphicon glyphicon-camera"></span>
+            <span>Lista de imagenes</span>
+            <div class="pull-right">
+              <form class="form-inline" action="media.php" method="POST" enctype="multipart/form-data">
+              <div class="form-group">
+                <div class="input-group">
+                  <span class="input-group-btn">
+                    <input type="file" name="file_upload" multiple="multiple" class="btn btn-primary btn-file"/>
+                 </span>
+
+                 <button type="submit" name="submit" class="btn btn-default">Subir</button>
+               </div>
+              </div>
+             </form>
+            </div>
+          </div>
+          <div class="panel-body">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th class="text-center" style="width: 50px;">#</th>
+                  <th class="text-center">Imagen</th>
+                  <th class="text-center">Descripción</th>
+                  <th class="text-center" style="width: 20%;">Tipo</th>
+                  <th class="text-center" style="width: 50px;">Acciones</th>
+                </tr>
+              </thead>
+                <tbody>
+                <?php foreach ($media_files as $media_file): ?>
+                <tr class="list-inline">
+                 <td class="text-center"><?php echo count_id();?></td>
+                  <td class="text-center">
+                      <img src="uploads/products/<?php echo $media_file['file_name'];?>" class="img-thumbnail" />
+                  </td>
+                <td class="text-center">
+                  <?php echo $media_file['file_name'];?>
+                </td>
+                <td class="text-center">
+                  <?php echo $media_file['file_type'];?>
+                </td>
+                <td class="text-center">
+                  <a href="delete_media.php?id=<?php echo (int) $media_file['id'];?>" class="btn btn-danger btn-xs"  title="Eliminar">
+                    <span class="glyphicon glyphicon-trash"></span>
+                  </a>
+                </td>
+               </tr>
+              <?php endforeach;?>
+            </tbody>
+          </div>
+        </div>
+      </div>
+</div>
+
