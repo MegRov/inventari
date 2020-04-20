bac90d24d2aad1f7e92ce8295425e501bb39f5ae
Miguel Guevara <lxmxg.96@outlook.com>
2020-04-20 10:53:03 -0500

    Variables css

--- layouts/header.php
+++ layouts/header.php
@@ -3,23 +3,27 @@
   <html lang="en">
     <head>
     <meta charset="UTF-8">
-    <link rel="shortcut icon" href="../libs/css/images/favicon.jpg" />
-
-    <title><?php if (!empty($page_title))
+    
+    
+    <title>
+      
+      <?php if (!empty($page_title))
            echo remove_junk($page_title);
-            elseif(!empty($user))
+           elseif(!empty($user))
            echo ucfirst($user['name']);
-            else echo "Sistema simple de inventario";?>
+           else echo "Sistema simple de inventario";?>
     </title>
-	
+    
+    <link rel="icon" type="image/png" href="../libs/images/favicon.png" sizes="16x16">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" />
     <link rel="stylesheet" href="libs/css/main.css" />
-  </head>
-  <body>
+  
   <?php  if ($session->isUserLoggedIn(true)): ?>
     <header id="header">
-      <div class="logo pull-left"> fruver - Inventory </div>
+      <div class="logo pull-left">Inventory         
+        <img src="../libs/images/favicon.png" alt="logo" sizes="16x16">
+      </div>
       <div class="header-content">
       <div class="header-date pull-left">
         <strong><?php echo date("d/m/Y  g:i a");?></strong>
@@ -54,24 +58,27 @@
           </li>
         </ul>
       </div>
-     </div>
+    </div>
     </header>
     <nav class="navbar sidebar d-flex justify-content-center">
       <?php if($user['user_level'] === '1'): ?>
         <!-- admin menu -->
-      <?php include_once('admin_menu.php');?>
-
-      <?php elseif($user['user_level'] === '2'): ?>
-        <!-- Special user -->
-      <?php include_once('special_menu.php');?>
-
-      <?php elseif($user['user_level'] === '3'): ?>
-        <!-- User menu -->
-      <?php include_once('user_menu.php');?>
-
-      <?php endif;?>
-   </nav>
-<?php endif;?>
+        <?php include_once('admin_menu.php');?>
+        
+        <?php elseif($user['user_level'] === '2'): ?>
+          <!-- Special user -->
+          <?php include_once('special_menu.php');?>
+          
+          <?php elseif($user['user_level'] === '3'): ?>
+            <!-- User menu -->
+            <?php include_once('user_menu.php');?>
+            
+            <?php endif;?>
+          </nav>
+          <?php endif;?>
+</head>
 
 <div class="page">
   <div class="container-fluid">
+
+          <body>
